d8b5eeccaab603da6b1f2f97cc5e0c5c4737c571
Mohib Arsala <mohib.0008@gmail.com>
2020-04-11 01:42:55 -0400

    robots file added as well destructirized code

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
-        <div>
-          <img alt="RoboV1" src="" />
+        <div className= " tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+          <img alt="RoboV1" src={`https://www.robohash.org/${id}?150/150`}/>
           <div>
-              <h2>Jane Doe</h2>
-              <p>jane.doe@yahoo.com</p>
+              <h2>{name}</h2>
+              <p>{email}</p>
           </div>
         </div>
     );
