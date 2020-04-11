6d24c7ef75e22ebc4f9c77ccdc3c1a3bfe04ef3b
Mohib Arsala <mohib.0008@gmail.com>
2020-04-11 00:03:29 -0400

    file add roboPhoto

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+          <img alt="RoboV1" src="" />
+          <div>
+              <h2>Jane Doe</h2>
+              <p>jane.doe@yahoo.com</p>
+          </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import "tachyons";
 
-ReactDOM.render(<Hello greating={"This is " + "NSP/ National Spy Programe + MRRD"} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Card />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
